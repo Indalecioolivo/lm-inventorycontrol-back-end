1efdded7d1339282623fd43bb01c153ae513dc65
Indalecio Olivo <indalecioolivo@gmail.com>
2024-02-20 14:15:23 -0300

    feat: crud products implemented

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,4 +1,12 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { ProductsService } from './products.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
@@ -8,8 +16,8 @@ export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
 
   @Post()
-  create(@Body() createProductDto: CreateProductDto) {
-    return this.productsService.create(createProductDto);
+  create(@Body() data: CreateProductDto) {
+    return this.productsService.create(data);
   }
 
   @Get()
@@ -17,14 +25,14 @@ export class ProductsController {
     return this.productsService.findAll();
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.productsService.findOne(+id);
+  @Get(':barcode')
+  findOne(@Param('barcode') barcode: string) {
+    return this.productsService.findOne(barcode);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateProductDto: UpdateProductDto) {
-    return this.productsService.update(+id, updateProductDto);
+  update(@Param('id') id: string, @Body() data: UpdateProductDto) {
+    return this.productsService.update(+id, data);
   }
 
   @Delete(':id')
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,26 +1,78 @@
 import { Injectable } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { PrismaService } from 'src/database/prisma.service';
 
 @Injectable()
 export class ProductsService {
-  create(createProductDto: CreateProductDto) {
-    return 'This action adds a new product';
+  constructor(private prisma: PrismaService) {}
+  async create(data: CreateProductDto): Promise<CreateProductDto> {
+    const exist = await this.prisma.product.findUnique({
+      where: {
+        bar_code: data.bar_code,
+      },
+    });
+    if (exist) {
+      throw new Error('This Product Already Exists');
+    }
+    return await this.prisma.product.create({
+      data: { ...data },
+    });
   }
 
-  findAll() {
-    return `This action returns all products`;
+  async findAll() {
+    return this.prisma.product.findMany();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} product`;
+  async findOne(barcode: string): Promise<CreateProductDto> | null {
+    const exist = await this.prisma.product.findUnique({
+      where: { bar_code: barcode },
+    });
+
+    if (!exist) {
+      throw new Error('This Product does not exist');
+    }
+
+    return exist;
   }
 
-  update(id: number, updateProductDto: UpdateProductDto) {
-    return `This action updates a #${id} product`;
+  async update(
+    id: number,
+    data: UpdateProductDto,
+  ): Promise<UpdateProductDto> | null {
+    const exist = await this.prisma.product.findUnique({
+      where: { id },
+    });
+    if (!exist) {
+      throw new Error('This Product does not exist');
+    }
+    if (
+      !data.bar_code &&
+      !data.description &&
+      !data.name &&
+      !data.stock &&
+      !data.volume
+    ) {
+      throw new Error('Enter at least one field to edit');
+    }
+    return await this.prisma.product.update({
+      where: { id },
+      data: { ...data },
+    });
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} product`;
+  async remove(id: number) {
+    const exist = await this.prisma.product.findUnique({
+      where: { id },
+    });
+
+    if (!exist) {
+      throw new Error('Product does not exist');
+    }
+
+    await this.prisma.product.delete({
+      where: { id },
+    });
+    return 'Product excluded with sucess';
   }
 }
